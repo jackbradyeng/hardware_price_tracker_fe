1e5abc7e6175674e86cc1d41aecf8b67bbb0de7a
jack-brady <jackbradyx@proton.me>
2026-03-25 23:25:55 +1000

    Add price point interfaces

---
+++ src/types/price_point_types/GPUPricePointType.ts
@@ -0,0 +1,8 @@
+export interface GPUPricePointType {
+    id: number | null;
+    modelNumber: string | null;
+    vendor: string | null;
+    currency: string | null;
+    price: number | null;
+    scrapedAt: string | null;
+}
---
+++ src/types/price_point_types/RAMPricePointType.ts
@@ -0,0 +1,8 @@
+export interface RAMPricePointType {
+    id: number | null;
+    modelNumber: string | null;
+    vendor: string | null;
+    currency: string | null;
+    price: number | null;
+    scrapedAt: string | null;
+}
