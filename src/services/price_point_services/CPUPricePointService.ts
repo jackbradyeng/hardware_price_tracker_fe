2381e0b11149c0d85a6fa2c72cfd484a0635d9ba
jack-brady <jackbradyx@proton.me>
2026-03-28 19:49:56 +1000

    Add model number endpoint to existing price point services

--- src/services/price_point_services/CPUPricePointService.ts
+++ src/services/price_point_services/CPUPricePointService.ts
@@ -1,4 +1,5 @@
 import type { CPUPricePointType } from '@/types/price_point_types/CPUPricePointType.ts';
+import type { CPUDataAndPricePointType } from '@/types/hybrid_types/CPUDataAndPricePointType.ts';
 
 export const getCPUPricePoints = async (): Promise<CPUPricePointType[]> => {
 
@@ -9,4 +10,16 @@ export const getCPUPricePoints = async (): Promise<CPUPricePointType[]> => {
     }
 
     return (await response.json()) as CPUPricePointType[];
+}
+
+export const getCPUPricePointsByModel
+    = async (modelNumber: string): Promise<CPUDataAndPricePointType> => {
+
+    const response = await fetch(`/api/cpu_pricepoints/${modelNumber}`);
+
+    if (!response.ok) {
+        throw new Error('Network response failed.');
+    }
+
+    return (await response.json()) as CPUDataAndPricePointType;
 }
--- src/services/price_point_services/GPUPricePointService.ts
+++ src/services/price_point_services/GPUPricePointService.ts
@@ -1,4 +1,5 @@
 import type { GPUPricePointType } from '@/types/price_point_types/GPUPricePointType.ts';
+import type { GPUDataAndPricePointType } from '@/types/hybrid_types/GPUDataAndPricePointType.ts';
 
 export const getGPUPricePoints = async (): Promise<GPUPricePointType[]> => {
 
@@ -9,4 +10,16 @@ export const getGPUPricePoints = async (): Promise<GPUPricePointType[]> => {
     }
 
     return (await response.json()) as GPUPricePointType[];
+}
+
+export const getGPUPricePointsByModel
+    = async (modelNumber: string): Promise<GPUDataAndPricePointType> => {
+
+    const response = await fetch(`/api/gpu_pricepoints/${modelNumber}`);
+
+    if (!response.ok) {
+        throw new Error('Network response failed.');
+    }
+
+    return (await response.json()) as GPUDataAndPricePointType;
 }
