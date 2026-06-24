951c0f6a1ff2969049a803f9e3aebd9cfdda822d
jack-brady <jackbradyx@proton.me>
2026-06-24 20:50:02 +1000

    Delete unused price point types

--- src/types/price_point_types/RAMPricePointType.ts
+++
@@ -1,8 +0,0 @@
-export interface RAMPricePointType {
-    id: number | null;
-    modelNumber: string | null;
-    vendor: string | null;
-    currency: string | null;
-    price: number | null;
-    scrapedAt: string | null;
-}
